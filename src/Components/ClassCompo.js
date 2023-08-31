6b743030ca7f3c66aab4f96f45a3cf4659026a55
mukeshnagila <nagilamukesh43@gmail.com>
2023-08-31 23:08:39 +0530

    reacthandson2commit2

--- src/Components/ClassCompo.js
+++ src/Components/ClassCompo.js
@@ -18,7 +18,16 @@ class ClassCompo extends Component{
         // console.log([e.target.name]);
     }
 
-    DataSubmit = () => {
+    DataSubmit = (e) => {
+        e.preventDefault()
+
+        const { name, department, rating } = this.state;
+
+        if (!name || !department || !rating) {
+            alert("Please fill out all fields before submitting.");
+            return;
+        }
+
         const newObj = {
             name : this.state.name,
             department : this.state.department,
@@ -26,7 +35,10 @@ class ClassCompo extends Component{
         }
         this.state.detaStore.push(newObj)
         this.setState({
-            detaStore : this.state.detaStore
+            detaStore : this.state.detaStore,
+            name : "",
+            department : "",
+            rating : ""
         })
     }
 
@@ -35,16 +47,18 @@ class ClassCompo extends Component{
             <>
                 <div>
                     <div className="form">
+                        <form>
                             <label htmlFor="name">Name: </label>
-                            <input type="text" id="name" name="name" placeholder="Enter Your Name" onChange={this.handlechange} required/><br/>
+                            <input type="text" id="name" name="name" value={this.state.name} placeholder="Enter Your Name" onChange={this.handlechange} required/><br/>
 
                             <label htmlFor="deprt">Department: </label>
-                            <input type="text" id="deprt" name="department" placeholder="Enter Your Department" onChange={this.handlechange} required/><br/>
+                            <input type="text" id="deprt" name="department" value={this.state.department} placeholder="Enter Your Department" onChange={this.handlechange} required/><br/>
 
                             <label htmlFor="num">Rating: </label>
-                            <input type="number" id="num" name="rating" placeholder="Enter Your Rating" onChange={this.handlechange} required/><br/>
+                            <input type="number" id="num" name="rating" value={this.state.rating} placeholder="Enter Your Rating" onChange={this.handlechange} required/><br/>
 
                             <button onClick={this.DataSubmit}>Submit</button>
+                        </form>    
                     </div>
 
                         <div className="main">
